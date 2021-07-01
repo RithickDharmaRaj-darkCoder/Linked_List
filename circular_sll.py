38b20bbc4e8e39341f5b55df73fade2fb41777de
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-02 03:07:41 +0530

    Circular Singly Linked List Overview ...

---
+++ circular_sll.py
@@ -0,0 +1,28 @@
+# Data Structure ...
+# User-Defined ...
+# Linked List ...
+# Circular Linked List ...
+# Circular-Singly Linked List ...
+
+class create_node:
+    def __init__(self,data):
+        self.data = data
+        self.linktoN = None
+
+class C_linkedlist:
+    def __init__(self,object_name):
+        self.name = object_name
+        self.head = None
+
+    def traversal(self):
+        pass
+    def add_at_starting(self,data):
+        pass
+    def add_at_ending(self,data):
+        pass
+    def add_after_node(self,data,x):
+        pass
+    def add_before_node(self,data,x):
+        pass
+
+
